99c97a2cad5b52882149c3e654fafe8b758506a9
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-26 11:11:33 +0100

    feat(carrera): Añadir clase Carrera

---
+++ src/main/java/edu/estatuas/cotxox/carrera/Carrera.java
@@ -0,0 +1,85 @@
+package edu.estatuas.cotxox.carrera;
+
+import edu.estatuas.cotxox.tarifa.Tarifa;
+
+public class Carrera {
+    
+    private String tarjetaCredito;
+    private String origen;
+    private String destino;
+    private double distancia;
+    private int tiempoEsperado;
+    private int tiempoCarrera;
+    private double costeTotal;
+    private int propina;
+
+    public Carrera(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    //region Getters and setter
+    public String getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setTiempoEsperado(int tiempoEsperado) {
+        this.tiempoEsperado = tiempoEsperado;
+    }
+
+    public int getTiempoEsperado() {
+        return tiempoEsperado;
+    }
+
+    public void setTiempoCarrera(int tiempoCarrera) {
+        this.tiempoCarrera = tiempoCarrera;
+    }
+
+    public int getTiempoCarrera() {
+        return tiempoCarrera;
+    }
+
+    public void setCosteTotal(double costeTotal) {
+        this.costeTotal = costeTotal;
+    }
+
+    public double getCosteTotal() {
+        return costeTotal;
+    }
+
+    public void setPropina(int propina) {
+        this.propina = propina;
+    }
+
+    public int getPropina() {
+        return propina;
+    }
+
+    public double getCosteEsperado() {
+        return Tarifa.getCosteTotalEsperado(this);
+    }
+
+}
