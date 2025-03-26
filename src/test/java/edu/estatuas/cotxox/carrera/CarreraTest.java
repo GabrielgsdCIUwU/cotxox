842352ae2df38034dfd49f74df3cf60561e34beb
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-26 11:11:13 +0100

    test(carrera): Añadir casos test de la clase Carrera

---
+++ src/test/java/edu/estatuas/cotxox/carrera/CarreraTest.java
@@ -0,0 +1,85 @@
+package edu.estatuas.cotxox.carrera;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CarreraTest {
+    
+    private Carrera carrera;
+
+    @Before
+    public void setUp() {
+        carrera = new Carrera("1234567890123456");
+    }
+
+    @Test
+    public void testGetTarjetaCredito() {
+        assertEquals("1234567890123456", carrera.getTarjetaCredito());
+    }
+
+    @Test
+    public void testSetGetOrigen() {
+        String origen = "Baiona, Bayona, Pontevedra";
+        carrera.setOrigen(origen);
+
+        assertEquals(origen, carrera.getOrigen());
+    }
+
+    @Test
+    public void testSetGetDestino() {
+        String destino = "Bayona, Francia";
+        carrera.setDestino(destino);
+
+        assertEquals(destino, carrera.getDestino());
+    }
+
+    @Test
+    public void testSetGetDistancia() {
+        double distancia = 828.54;
+        carrera.setDistancia(distancia);
+
+        assertEquals(distancia, carrera.getDistancia(), 0.01);
+    }
+
+    @Test
+    public void testSetGetTiempoEsperado() {
+        int tiempoEsperado = 500;
+        carrera.setTiempoEsperado(tiempoEsperado);
+
+        assertEquals(tiempoEsperado, carrera.getTiempoEsperado());
+    }
+
+    @Test
+    public void testSetGetTiempoCarrera() {
+        int tiempoCarrera = 487;
+        carrera.setTiempoCarrera(tiempoCarrera);
+
+        assertEquals(tiempoCarrera, carrera.getTiempoCarrera());
+    }
+
+    @Test
+    public void testSetGetCosteTotal() {
+        double costeTotal = 1499.99;
+        carrera.setCosteTotal(costeTotal);
+
+        assertEquals(costeTotal, carrera.getCosteTotal(), 0.01);
+    }
+
+    @Test
+    public void testSetGetPropina() {
+        int propina = 100;
+        carrera.setPropina(propina);
+
+        assertEquals(propina, carrera.getPropina());
+    }
+
+   @Test
+   public void testCosteEsperado() {
+        carrera.setDistancia((double) 200.5);
+        carrera.setTiempoEsperado( 28);
+
+        assertEquals(336.57, carrera.getCosteEsperado(), 0.01);
+   }
+}
