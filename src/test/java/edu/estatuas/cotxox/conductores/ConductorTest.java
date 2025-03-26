e9832a925abfd974ac931f1e482b5dff5e2bd9ab
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-26 10:18:27 +0100

    test(conductor): Añadir casos test de conductor

---
+++ src/test/java/edu/estatuas/cotxox/conductores/ConductorTest.java
@@ -0,0 +1,38 @@
+package edu.estatuas.cotxox.conductores;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConductorTest {
+    private Conductor conductor;
+
+    @Before
+    public void setUp() {
+        conductor = new Conductor("Juan");
+    }
+
+    @Test
+    public void testNombre() {
+        assertEquals("Juan", conductor.getNombre());
+    }
+
+    @Test
+    public void testModelo() {
+        conductor.setModelo("Chevy Malibu");
+        assertEquals("Chevy Malibu", conductor.getModelo());
+    }
+
+    @Test
+    public void testMatricula() {
+        conductor.setMatricula("4ABC123");
+        assertEquals("4ABC123", conductor.getMatricula());
+    }
+
+    @Test
+    public void testValoracion() {
+        conductor.setValoracion((byte) 0);
+        assertEquals((byte) 0, conductor.getValoracion(), 0.01);
+    }
+}
